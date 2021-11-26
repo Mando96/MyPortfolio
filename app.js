f6ef43e582da19f2d6e6ce4d06051d56b7bc109d
Oluwaseun Okeowo <oluwaseunokeowo@Oluwaseuns-MacBook-Air.local>
2021-11-26 13:38:45 -0500

    Update to index.html and style.css

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const text = document.getElementsByClassName('text')
+
+
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -25,5 +28,7 @@ const navSlide = () => {
  });
    
 }
+
+
 
 navSlide();
